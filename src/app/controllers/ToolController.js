702d16bbab3572b54fb8b551eb1bd3d054ddc3c2
Paulo Henrique Ferreira <paulohfcmendes@gmail.com>
2019-08-13 14:02:08 -0300

    feat/ SHOW (by tag) method

--- src/app/controllers/ToolController.js
+++ src/app/controllers/ToolController.js
@@ -3,9 +3,15 @@ import Tool from "../models/Tool";
 class ToolController {
   //GET (list)
   async index(req, res) {
+    console.log(req.query.tags)
     try {
-      const tools = await Tool.find();
 
+      if (req.query.tags) {
+        const tools = await Tool.find({ tags: { $all: req.query.tags } })
+
+        return res.json(tools);
+      }
+      const tools = await Tool.find();
       return res.status(200).json(tools);
     } catch (err) {
       return res.status(400).json({ error: err });
@@ -18,7 +24,7 @@ class ToolController {
       const tool = await Tool.create(req.body);
 
       return res.status(200).json({ tool });
-    } catch (err) {}
+    } catch (err) { }
   }
 }
 
